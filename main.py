1a5e5098e8c84f52981279534a66efc33f1d862b
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-05 14:10:51 -0800

    Created find_slope. Still working with Atom, seeing if GitHub Desktop needed.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # Pathfinder - Aditya Saravanan
 # A program for finding the shortest path between two different points.
 
-def create_line(start_point, end_point):
-    
+def find_slope(start_point, end_point):
+    return (end_point[1] - start_point[1])/(end_point[0] - start_point[0])
+
+def
+
+
+
+
+# idea: make a line which sections the area where the point can actually move
+# if the point moves outside this area, that path is considered null and void.
