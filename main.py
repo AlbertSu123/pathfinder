d521c980a443817e898784b41e90e4170a1f47c0
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-05 18:26:31 -0800

    Basic functions for searches.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # Pathfinder - Aditya Saravanan
 # A program for finding the shortest path between two different points.
 
-def find_slope(start_point, end_point):
-    return (end_point[1] - start_point[1])/(end_point[0] - start_point[0])
+# Meant to check if the point is on the board_size_x by board_size_y grid contained in the first quadrant
+def check_if_on_board(point, board_size_x, board_size_y):
+    if point[0] > board_size_x or point[0] < 0 or point[1] > board_size_y or point[1] < 0:
+        return False
+    return True
 
-def
-
-
-
-
-# idea: make a line which sections the area where the point can actually move
-# if the point moves outside this area, that path is considered null and void.
+# Returns a list of all possible valid movements from the current position.
+def possible_movements(point, board_size_x, board_size_y):
+    movement_lst = []
+    movement_lst.append([point[0] + 1, point[1]])
+    movement_lst.append([point[0] - 1, point[1]])
+    movement_lst.append([point[0], point[1] + 1])
+    movement_lst.append([point[0], point[1] - 1])
+    return [point for point in movement_lst if check_if_on_board(point, board_size_x, board_size_y)]
