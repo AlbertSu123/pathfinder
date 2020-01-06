7ae7d4b14995d9a4abb3cf82e3f007cc9a21dc39
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-05 20:43:03 -0800

    Final commit for basic board structure. Needs some time fixing, can do later.

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 # Pathfinder - Aditya Saravanan
 # A program for finding the shortest path between two different points.
 
-
-
 class Node:
 
     can_use = True
-    connections = []
 
     def __init__(self, x_pos, y_pos):
         self.coordinates = [x_pos, y_pos]
-        node.x_coordinate = x_pos
-        node.y_coordinate = y_pos
+        self.x_coordinate = x_pos
+        self.y_coordinate = y_pos
+        self.connections = []
 
-    def check_if_neighbors(node1, node2):
+    def check_node_neighbors(node1, node2):
         if node1.x_coordinate == node2.x_coordinate:
             if abs(node1.y_coordinate - node2.y_coordinate) == 1:
                 return True
             return False
-        elif node1.y_coordinate == node2.y_coodinate:
-            if abs(node1.x_coordinate - node2.x_coodinate) == 1:
+        elif node1.y_coordinate == node2.y_coordinate:
+            if abs(node1.x_coordinate - node2.x_coordinate) == 1:
                 return True
             return False
         return False
@@ -39,5 +37,5 @@ class Board:
     def make_connections(self):
         for node in self.values:
             for other_node in self.values:
-                if check_if_neighbors(node, other_node):
+                if Node.check_node_neighbors(node, other_node):
                     node.connections.append(other_node)
