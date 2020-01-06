060e64ee1f5064e788a79299d72e8c4b3d35f6e6
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-05 19:51:06 -0800

    Totally reworked beginning functions. Used OOP with board.

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 # Pathfinder - Aditya Saravanan
 # A program for finding the shortest path between two different points.
 
-# Meant to check if the point is on the board_size_x by board_size_y grid contained in the first quadrant
-def check_if_on_board(point, board_size_x, board_size_y):
-    if point[0] > board_size_x or point[0] < 0 or point[1] > board_size_y or point[1] < 0:
-        return False
-    return True
 
-# Returns a list of all possible valid movements from the current position.
-def possible_movements(point, board_size_x, board_size_y):
-    movement_lst = []
-    movement_lst.append([point[0] + 1, point[1]])
-    movement_lst.append([point[0] - 1, point[1]])
-    movement_lst.append([point[0], point[1] + 1])
-    movement_lst.append([point[0], point[1] - 1])
-    return [point for point in movement_lst if check_if_on_board(point, board_size_x, board_size_y)]
+
+class Node:
+
+    can_use = True
+    connections = []
+
+    def __init__(self, x_pos, y_pos):
+        self.coordinates = [x_pos, y_pos]
+        node.x_coordinate = x_pos
+        node.y_coordinate = y_pos
+
+    def check_if_neighbors(node1, node2):
+        if node1.x_coordinate == node2.x_coordinate:
+            if abs(node1.y_coordinate - node2.y_coordinate) == 1:
+                return True
+            return False
+        elif node1.y_coordinate == node2.y_coodinate:
+            if abs(node1.x_coordinate - node2.x_coodinate) == 1:
+                return True
+            return False
+        return False
+
+class Board:
+
+    def __init__(self, x_size, y_size):
+        self.values = []
+        self.fill_board(x_size, y_size)
+        self.make_connections()
+
+    def fill_board(self, x_size, y_size):
+        for xcounter in range(x_size+1):
+            for ycounter in range(y_size+1):
+                self.values.append(Node(xcounter, ycounter))
+
+    def make_connections(self):
+        for node in self.values:
+            for other_node in self.values:
+                if check_if_neighbors(node, other_node):
+                    node.connections.append(other_node)
