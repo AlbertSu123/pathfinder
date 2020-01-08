ee09fbca73b411c23933b986562ef71c2cc1c147
adisaran64 <59209660+adisaran64@users.noreply.github.com>
2020-01-07 17:09:10 -0800

    Final changes. Changed name from gui.py to visuals.py, added visuals.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from functions import *
 from classes import *
+from visuals import *
 import time
 
 
@@ -10,10 +11,10 @@ import time
 
 def board_creation():
     """Creates the board that will be used, with user-specified dimensions."""
-    x_size = int(input("How long would you like the x-axis of your board to be? The maximum should be 100. "))
-    y_size = int(input("\nHow long would you like the y-axis of your board to be? The maximum should be 100. "))
-    if x_size > 100 or y_size > 100:
-        print("Sorry, the size of the board is too big. Please enter new dimensions.")
+    x_size = int(input("\nHow long would you like the x-axis of your board to be? The value should be between 2 and 100. "))
+    y_size = int(input("\nHow long would you like the y-axis of your board to be? The value should be between 2 and 100. "))
+    if x_size > 100 or y_size > 100 or x_size < 2 or y_size < 2:
+        print("Sorry, the size of the board is unattainable. Please enter new dimensions.")
         return board_creation()
     walls = wall_creation(x_size, y_size)
     return Board(x_size, y_size, walls)
@@ -48,7 +49,7 @@ def choose_start_and_end_points(board):
     x2 = int(input("\nPlease type the x-value for your desired endpoint. Remember, your endpoint cannot be your start point. "))
     y2 = int(input("\nPlease type the y-value for your desired endpoint. Remember, your endpoint cannot be your start point. "))
     if not board.find_node_from_coordinates([x2, y2]):
-        print("\nSorry, that startpoint is invalid. Either it's not on the board, or it's a position of a wall. Choose another point, please.\n")
+        print("\nSorry, that endpoint is invalid. Either it's not on the board, or it's a position of a wall. Choose another point, please.\n")
         return choose_start_and_end_points(board)
     if [x1, y1] == [x2, y2]:
         print("\nYou chose the same startpoint and endpoint. Please try again.\n")
@@ -63,10 +64,13 @@ def process_result(start_point, end_point, algorithm, board):
     time_taken = time.process_time() - start_time
     if not result or len(result) == 1:
         print("\nNo path could be found by the algorithm. Either this algorithm is not suited for the task or the startpoint/endpoint cannot be reached due to walls.")
+        print("\nPlease see the other window for a picture of the board.")
+        result = []
     else:
         print("\nThis was the path found by the algorithm: ", result)
         print("\nThis was the amount of time that it took the algorithm to find the path: ", time_taken, " seconds")
-    waiting = input("\nPress enter once you are ready to continue. ")
+        print("\nPlease see the other window for a picture of the board. Exit the window to continue.")
+    draw_board(board, result)
 
 
 ### Execution Functions ###
@@ -90,10 +94,10 @@ def restart(board):
     """Function used in mutual recursion with the loop() function to allow the
     user to choose whether to keep their board or generate a new one after an
     algorithm runs."""
-    restart = input("\nWould you like to keep the same board? Type 'keep'. Otherwise, to restart, type 'restart'. Use Ctrl + C to exit. ")
-    if "k" in restart:
+    want_to_restart = input("\nWould you like to keep the same board? Type 'keep'. Otherwise, to restart, type 'restart'. Use Ctrl + C, then Enter, to exit. ")
+    if "k" in want_to_restart:
         loop(board)
-    elif "res" in restart:
+    elif "res" in want_to_restart:
         loop()
     else:
         print("\nSorry, what you typed didn't make sense. Make sure to pick one of the options above.\n")
@@ -112,7 +116,7 @@ def main():
          | |_) / _` | __| '_ \| |_| | '_ \ / _` |/ _ \ '__|
          |  __/ (_| | |_| | | |  _| | | | | (_| |  __/ |
          |_|   \__,_|\__|_| |_|_| |_|_| |_|\__,_|\___|_|
-                                                   \n""")
+                                                    """)
     loop()
 
 main()
